e884d83a1ed9ae7e853faa854662afea47617710
Alexander Watson <zredlined@users.noreply.github.com>
2022-02-08 10:03:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+import pathlib
 from setuptools import setup, find_packages
 
-setup(name="trainer", package_dir={'': 'src'}, packages=find_packages("src"))
+local_path = pathlib.Path(__file__).parent
+install_requires = (local_path / "requirements.txt").read_text().splitlines()
+
+setup(name="trainer", package_dir={'': 'src'}, install_requires=install_requires, packages=find_packages("src"))
