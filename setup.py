f2b06294c68d928482ff80e5ea3796702081a0b3
Alexander Watson <zredlined@users.noreply.github.com>
2022-02-08 14:45:14 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ from setuptools import setup, find_packages
 local_path = pathlib.Path(__file__).parent
 install_requires = (local_path / "requirements.txt").read_text().splitlines()
 
-setup(name="trainer", package_dir={'': 'src'}, install_requires=install_requires, packages=find_packages("src"))
+setup(name="trainer",
+      version="0.0.1",
+      package_dir={'': 'src'}, 
+      install_requires=install_requires, 
+      packages=find_packages("src")
+)
