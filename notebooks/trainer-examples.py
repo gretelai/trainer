f500e0bb894f8724cc02eabe6d56aab572d2ac1a
Alexander Watson <zredlined@users.noreply.github.com>
2022-07-29 10:20:05 -0700

    update examples

--- notebooks/trainer-examples.py
+++ notebooks/trainer-examples.py
@@ -1,4 +1,4 @@
-from gretel_trainer import trainer, runner
+from gretel_trainer import trainer
 
 dataset = "https://gretel-public-website.s3-us-west-2.amazonaws.com/datasets/USAdultIncome5k.csv"
 
@@ -6,24 +6,23 @@ dataset = "https://gretel-public-website.s3-us-west-2.amazonaws.com/datasets/USA
 model = trainer.Trainer()
 model.train(dataset)
 df = model.generate()
+print(df)
 
-# Specify underlying model
-#model = trainer.Trainer(model_type="GretelLSTM")
+# Or, Specify underlying model and config options.
+# Valid options are "GretelLSTM", "GretelCTGAN"
+
+#model = trainer.Trainer(
+#    model_type="GretelCTGAN",
+#    max_header_clusters=100,
+#    max_rows=50000,
+#    model_params={"epochs": 600, "num_records": 5500},
+#)
 #model.train(dataset)
 #df = model.generate()
+#print(df)
 
-# Update trainer parameters
-#model = trainer.Trainer(max_header_clusters=20, max_rows=50000)
-#model.train(dataset)
-#df = model.generate()
+# Or, load and generate data from an existing model
 
-# Specify synthetic model and update config params
-#model = trainer.Trainer(model_type="GretelCTGAN", model_params={'epochs':2})
-#model.train(dataset)
-#df = model.generate()
-
-# Load and generate data from an existing model
 #model = trainer.Trainer.load()
 #df = model.generate(num_records=70)
-
-print(df)
+#print(df)
