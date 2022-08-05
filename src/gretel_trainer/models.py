3586847eaa08a9bad0b027ba70630acefa0a073b
Alexander Watson <zredlined@users.noreply.github.com>
2022-08-05 10:17:36 -0700

    self-contained model base classes

--- src/gretel_trainer/models.py
+++ src/gretel_trainer/models.py
@@ -1,81 +1,68 @@
-"""Model specific parameters and definitions"""
-
-from collections import namedtuple
-from enum import Enum
-
-
-class BaseConfig:
+class _BaseConfig:
     """This class should not be used directly, model-specific configs should be
     derived from this class
     """
 
+    _max_header_clusters: int
+    """This should be overridden on concrete classes"""
+
+    # Default values for all configs, may be overridden if needed
+    _max_rows: int = 1000000
+
+    # Should be set by concrete constructors
+    config_file: str
+    max_rows: int
+    max_header_clusters: int
+
     def __init__(
         self,
         config_file: str,
-        default_rows: int,
         max_rows: int,
-        default_header_clusters: int,
         max_header_clusters: int,
     ):
         self.config_file = config_file
-        self.default_rows = default_rows
         self.max_rows = max_rows
-        self.default_header_clusters = default_header_clusters
         self.max_header_clusters = max_header_clusters
 
+        self.validate()
 
-class GretelLSTMConfig(BaseConfig):
-    def __init__(self):
+    def validate(self):
+        if self.max_rows > self._max_rows:
+            raise ValueError("too many rows")
+
+        if self.max_header_clusters > self._max_header_clusters:
+            raise ValueError("too many header clusters")
+
+
+class GretelLSTMConfig(_BaseConfig):
+
+    _max_header_clusters: int = 30
+
+    def __init__(
+        self,
+        config_file="synthetics/default",
+        max_rows=50000,
+        max_header_clusters=20,
+    ):
         super().__init__(
-            config_file="synthetics/default",
-            default_rows=50000,
-            max_rows=1000000,
-            default_header_clusters=20,
-            max_header_clusters=30,
+            config_file=config_file,
+            max_rows=max_rows,
+            max_header_clusters=max_header_clusters,
         )
 
 
-class GretelCTGANConfig(BaseConfig):
-    def __init__(self):
+class GretelCTGANConfig(_BaseConfig):
+
+    _max_header_clusters: int = 1000
+
+    def __init__(
+        self,
+        config_file="synthetics/high-dimensionality",
+        max_rows=50000,
+        max_header_clusters=500,
+    ):
         super().__init__(
-            config_file="synthetics/high-dimensionality",
-            default_rows=50000,
-            max_rows=1000000,
-            default_header_clusters=500,
-            max_header_clusters=1000,
+            config_file=config_file,
+            max_rows=max_rows,
+            max_header_clusters=max_header_clusters,
         )
-
-
-class ExtendedEnum(Enum):
-    """Utility class for Model enum"""
-
-    @classmethod
-    def get_types(cls):
-        return list(map(lambda c: c.name, cls))
-
-    @classmethod
-    def get_config(cls, model: str) -> str:
-        return cls[model].config.config_file
-
-    @classmethod
-    def get_default_rows(cls, model: str) -> int:
-        return cls[model].config.default_rows
-
-    @classmethod
-    def get_max_rows(cls, model: str) -> int:
-        return cls[model].config.max_rows
-
-    @classmethod
-    def get_default_header_clusters(cls, model: str) -> int:
-        return cls[model].config.default_header_clusters
-
-    @classmethod
-    def get_max_header_clusters(cls, model: str) -> int:
-        return cls[model].config.max_header_clusters
-
-
-class Model(namedtuple("Model", "config"), ExtendedEnum):
-    """Enum to pair default models and configurations"""
-
-    GretelLSTM = GretelLSTMConfig()
-    GretelCTGAN = GretelCTGANConfig()
