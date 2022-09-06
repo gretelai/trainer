d123fc945f3a1fe28c186c16ac6212c8a5aa1116
Alexander Watson <zredlined@users.noreply.github.com>
2022-09-06 14:42:34 -0700

    simplify trainer inits

--- notebooks/conditional-generation.py
+++ notebooks/conditional-generation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from gretel_trainer import trainer
+from gretel_trainer import Trainer
 from gretel_trainer.models import GretelLSTM, GretelCTGAN
 
 DATASET_PATH = 'https://gretel-public-website.s3.amazonaws.com/datasets/mitre-synthea-health.csv'
@@ -22,10 +22,10 @@ seed_df = pd.DataFrame(data=[
 
 # Train a model and conditionally generate data
 seed_fields = seed_df.columns.values.tolist()
-model = trainer.Trainer(model_type=MODEL_TYPE)
+model = Trainer(model_type=MODEL_TYPE)
 model.train(DATASET_PATH, seed_fields=seed_fields)
 print(model.generate(seed_df=seed_df))
 
 # Load a existing model and conditionally generate data
-#model = trainer.Trainer.load()
+#model = Trainer.load()
 #print(model.generate(seed_df=seed_df))
