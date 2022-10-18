a6dc6e892e05a1aecb9abe9af32e3597cc9f1080
Lipika Ramaswamy <lipika.ramaswamy@gmail.com>
2022-10-18 16:50:56 -0400

    logic for selecting model and new model types added

--- src/gretel_trainer/models.py
+++ src/gretel_trainer/models.py
@@ -9,23 +9,86 @@ if TYPE_CHECKING:
 from gretel_client.projects.models import read_model_config
 
 
-HIGH_COLUMN_THRESHOLD = 20
-HIGH_RECORD_THRESHOLD = 50000
-LOW_COLUMN_THRESHOLD = 4
-LOW_RECORD_THRESHOLD = 1000
+HIGH_RECORD_THRESHOLD = 1500000
+MEDIUM_RECORD_THRESHOLD_2 = 500000
+MEDIUM_RECORD_THRESHOLD_1 = 200000
+LOW_RECORD_THRESHOLD = 500
+
+HIGH_COLUMN_THRESHOLD = 2
+LOW_COLUMN_THRESHOLD = 1
 
 
 def determine_best_model(manifest: dict):
-    # TODO confirm manifest format, key names, nesting
+    """
+    [columns]
+
+        | l |                              |                               |
+        | o |     high-dimensionality      | high-dimensionality           |  a
+        | w |                              |    (low epochs)               |  m
+     20 |___|______________________________._______________________________.______
+        | r | if text or highly unique:    |                |              |  p
+        | e |   "complex-or-free-text"     |                |high-         |  l
+        | c | if max_precision > 3:        |                |dimensionality|  i
+        | o |   "high-dimensionality"      |                |  (low epochs)|  f
+        | r | if 50% columns are numeric:  |                |              |  y
+        | d |   "high-dimensionality"      |                |              |
+        |   | else:                        |  high-         |              |
+        |   |   default                    |  dimensionality|              |
+      1 |.__|______________________________.________________._____________.______
+        | GPT or Amplify                   |                |             |
+        |___.______________________________.________________._____________.______ [rows]
+        0    500                          200k             500k          1.5m
+
+    """
+
     row_count = manifest["record_count"]
     column_count = manifest["field_count"]
+    type_count = {
+        manifest["types"][k]["type"]: manifest["types"][k]["count"]
+        for k in range(len(manifest["types"]))
+    }
+    max_precision = max(
+        [x["max_precision"] for x in manifest["fields"] if "max_precision" in x.keys()]
+        + [0]
+    )
+    highly_unique_field_count = manifest["highly_unique_field_count"]
 
-    if row_count > HIGH_RECORD_THRESHOLD or column_count > HIGH_COLUMN_THRESHOLD:
-        return GretelCTGAN(config="synthetics/high-dimensionality")
-    elif row_count < LOW_RECORD_THRESHOLD or column_count < LOW_COLUMN_THRESHOLD:
-        return GretelCTGAN(config="synthetics/low-record-count")
-    else:
-        return GretelLSTM(config="synthetics/default")
+    if column_count <= LOW_COLUMN_THRESHOLD:
+        if type_count["text"] == LOW_COLUMN_THRESHOLD:
+            return GretelGPT(config="synthetics/natural-language")
+        else:
+            return GretelAmplify(config="synthetics/amplify")
+
+    elif row_count > HIGH_RECORD_THRESHOLD:
+        return GretelAmplify(config="synthetics/amplify")
+
+    elif row_count < LOW_RECORD_THRESHOLD:
+        return GretelCTGAN("synthetics/low-record-count")
+
+    elif column_count <= HIGH_COLUMN_THRESHOLD:
+        if row_count < MEDIUM_RECORD_THRESHOLD_1:
+            if type_count["other"] + type_count["text"] > 0:
+                return GretelLSTM("synthetics/complex-or-free-text")
+            elif highly_unique_field_count > 0:
+                return GretelLSTM("synthetics/complex-or-free-text")
+            elif max_precision > 2:
+                return GretelCTGAN("synthetics/high-dimensionality")
+            elif type_count["numeric"] / column_count > 0.5:
+                return GretelCTGAN("synthetics/high-dimensionality")
+            else:
+                return GretelLSTM("synthetics/default")
+        elif row_count < MEDIUM_RECORD_THRESHOLD_2:
+            ## change this based on either n_rows * n_cols or n_rows/n_cols
+            return GretelCTGAN("synthetics/high-dimensionality")
+        else:
+            ## change this based on either n_rows * n_cols or n_rows/n_cols
+            return GretelCTGAN("synthetics/high-dimensionality-high-records")
+    elif column_count > HIGH_COLUMN_THRESHOLD:
+        if row_count < MEDIUM_RECORD_THRESHOLD_1:
+            return GretelCTGAN("synthetics/high-dimensionality")
+        else:
+            ## change to high-dim-high-records when available
+            return GretelCTGAN("synthetics/high-dimensionality-high-records")
 
 
 class _BaseConfig:
@@ -56,7 +119,9 @@ class _BaseConfig:
         self.enable_privacy_filters = enable_privacy_filters
 
         if not self.enable_privacy_filters:
-            logging.warning("Privacy filters disabled. Enable with the `enable_privacy_filters` param.")
+            logging.warning(
+                "Privacy filters disabled. Enable with the `enable_privacy_filters` param."
+            )
             self.update_params({"outliers": None, "similarity": None})
 
         self.validate()
@@ -65,21 +130,25 @@ class _BaseConfig:
         """Convenience function to update model specific parameters from the base config by key value.
 
         Args:
-            params (dict): Dictionary of model parameters and values to update. E.g. {'epochs': 50}
+            params (dict): Dictionary of model parameters and values to update. E.g. {"epochs": 50}
         """
         # Update default config settings with params by key
         for key, value in params.items():
             self.config = self._replace_nested_key(self.config, key, value)
-        
+
     def validate(self):
-        if self._model_slug not in list(self.config['models'][0].keys()):
+        if self._model_slug not in list(self.config["models"][0].keys()):
             raise ValueError("Invalid configuration file selected for this model type")
 
         if self.max_rows > self._max_rows_limit:
-            raise ValueError(f"max_rows must be less than {self._max_rows_limit} for this model type.")
+            raise ValueError(
+                f"max_rows must be less than {self._max_rows_limit} for this model type."
+            )
 
         if self.max_header_clusters > self._max_header_clusters_limit:
-            raise ValueError(f"max_header_clusters must be less than {self._max_header_clusters_limit} for this model type.")
+            raise ValueError(
+                f"max_header_clusters must be less than {self._max_header_clusters_limit} for this model type."
+            )
 
     def _replace_nested_key(self, data, key, value) -> dict:
         """Replace nested keys"""
@@ -132,5 +201,39 @@ class GretelCTGAN(_BaseConfig):
             config=config,
             max_rows=max_rows,
             max_header_clusters=max_header_clusters,
+            enable_privacy_filters=enable_privacy_filters,
+        )
+
+
+class GretelGPT(_BaseConfig):
+
+    _max_header_clusters_limit: int = 1000
+    _max_rows_limit: int = 5000000
+    _model_slug: str = "gpt_x"
+
+    def __init__(
+        self,
+        config="synthetics/natural-language",
+        enable_privacy_filters=False,
+    ):
+        super().__init__(
+            config=config,
+            enable_privacy_filters=enable_privacy_filters,
+        )
+
+
+class GretelAmplify(_BaseConfig):
+
+    _max_header_clusters_limit: int = 1000
+    _max_rows_limit: int = 5000000
+    _model_slug: str = "amplify"
+
+    def __init__(
+        self,
+        config="synthetics/amplify",
+        enable_privacy_filters=False,
+    ):
+        super().__init__(
+            config=config,
             enable_privacy_filters=enable_privacy_filters,
         )
