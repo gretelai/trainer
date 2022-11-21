0010766d5553061c45766e9b7c1f66f98c269f29
Mike Knepper <mike@gretel.ai>
2022-11-21 15:00:17 -0600

    Join ancestors with lineage captured in column names

--- src/gretel_trainer/relational/relationships.py
+++ src/gretel_trainer/relational/relationships.py
@@ -1,38 +1,109 @@
-from __future__ import annotations
-from typing import List, Tuple
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
 
-import networkx as nx
+import networkx
+import pandas as pd
 
 
-class Relationships:
+@dataclass
+class ForeignKey:
+    column_name: str
+    parent_column_name: str
+    parent_table_name: str
+
+
+class RelationalData:
     def __init__(self):
-        self.g = nx.DiGraph()
+        self.graph = networkx.DiGraph()
+        self.lineage_column_delimiter = "|"
 
-    def all_tables(self) -> List[str]:
-        return list(self.g.nodes)
+    def add_table(self, table: str, primary_key: Optional[str], data: pd.DataFrame) -> None:
+        self.graph.add_node(table, primary_key=primary_key, data=data)
 
-    def add(self, fk: Tuple[str, str], referencing: Tuple[str, str]) -> Relationships:
-        fk_table, fk_column = fk
-        pk_table, pk_column = referencing
+    def add_foreign_key(self, foreign_key: str, referencing: str) -> None:
+        """Format of both str arguments should be `table_name.column_name`"""
+        fk_table, fk_column = foreign_key.split(".")
+        referenced_table, referenced_column = referencing.split(".")
+        self.graph.add_edge(fk_table, referenced_table)
+        edge = self.graph.edges[fk_table, referenced_table]
+        via = edge.get("via", [])
+        via.append(ForeignKey(
+            column_name=fk_column,
+            parent_column_name=referenced_column,
+            parent_table_name=referenced_table,
+        ))
+        edge["via"] = via
 
-        self.g.add_edge(
-            fk_table,
-            pk_table,
-            fk={"table": fk_table, "column": fk_column},
-            pk={"table": pk_table, "column": pk_column},
-        )
-        return self
+    def list_all_tables(self) -> List[str]:
+        return list(self.graph.nodes)
 
     def get_parents(self, table: str) -> List[str]:
-        return list(self.g.successors(table))
+        return list(self.graph.successors(table))
+
+    def get_primary_key(self, table: str) -> Optional[str]:
+        return self.graph.nodes[table]["primary_key"]
+
+    def get_table_data(self, table: str) -> pd.DataFrame:
+        return self.graph.nodes[table]["data"]
+
+    def get_foreign_keys(self, table: str) -> List[ForeignKey]:
+        foreign_keys = []
+        for parent in self.get_parents(table):
+            fks = self.graph.edges[table, parent]["via"]
+            foreign_keys.extend(fks)
+        return foreign_keys
+
+    def get_table_data_with_ancestors(self, table: str) -> pd.DataFrame:
+        """
+        Returns a data frame with all ancestral data joined to each record.
+        Column names are modified to the format `LINAGE|COLUMN_NAME`.
+        Lineage begins with `self` for the supplied `table`, and as older
+        generations are joined, the foreign keys to those generations are appended,
+        separated by periods.
+        """
+        lineage = "self"
+        df = self.get_table_data(table)
+        df = df.add_prefix(f"{lineage}{self.lineage_column_delimiter}")
+        return _join_parents(df, table, lineage, self)
+
+    def drop_ancestral_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        delim = self.lineage_column_delimiter
+        root_columns = [col for col in df.columns if col.startswith(f"self{delim}")]
+        mapper = { col: col.removeprefix(f"self{delim}") for col in root_columns }
+        return df[root_columns].rename(columns=mapper)
+
+
+def _join_parents(
+    df: pd.DataFrame,
+    table: str,
+    lineage: str,
+    relational_data: RelationalData
+) -> pd.DataFrame:
+    delim = relational_data.lineage_column_delimiter
+    for foreign_key in relational_data.get_foreign_keys(table):
+        next_lineage = f"{lineage}.{foreign_key.column_name}"
+
+        parent_table_name = foreign_key.parent_table_name
+        parent_data = relational_data.get_table_data(parent_table_name)
+        parent_data = parent_data.add_prefix(f"{next_lineage}{delim}")
+
+        df = df.merge(
+            parent_data,
+            how="left",
+            left_on=f"{lineage}{delim}{foreign_key.column_name}",
+            right_on=f"{next_lineage}{delim}{foreign_key.parent_column_name}",
+        )
+
+        df = _join_parents(df, parent_table_name, next_lineage, relational_data)
+    return df
 
 
 class TableProgress:
-    def __init__(self, relationships: Relationships):
-        self.relationships = relationships
+    def __init__(self, relational_data: RelationalData):
+        self.relational_data = relational_data
         self.table_statuses = {
             table: False
-            for table in relationships.all_tables()
+            for table in relational_data.list_all_tables()
         }
 
     def mark_complete(self, table: str) -> None:
@@ -44,7 +115,7 @@ class TableProgress:
             if processed:
                 continue
 
-            parents = self.relationships.get_parents(table)
+            parents = self.relational_data.get_parents(table)
             if len(parents) == 0:
                 ready.append(table)
                 continue
--- tests/test_relational.py
+++ tests/test_relational.py
@@ -1,15 +1,88 @@
-from gretel_trainer.relational.relationships import Relationships, TableProgress
+import pandas as pd
+
+from gretel_trainer.relational.relationships import RelationalData, TableProgress
+
+
+def _setup_ecommerce():
+    ecom = RelationalData()
+    ecom.add_table("events", "id", pd.DataFrame(columns=["id", "browser", "traffic_source", "user_id"]))
+    ecom.add_table("users", "id", pd.DataFrame(columns=["id", "first_name", "last_name"]))
+    ecom.add_table("inventory_items", "id", pd.DataFrame(columns=["id", "sold_at", "cost", "product_id", "product_distribution_center_id"]))
+    ecom.add_table("products", "id", pd.DataFrame(columns=["id", "name", "brand", "distribution_center_id"]))
+    ecom.add_table("distribution_center", "id", pd.DataFrame(columns=["id", "name"]))
+    ecom.add_table("order_items", "id", pd.DataFrame(columns=["id", "sale_price", "status", "user_id", "inventory_item_id"]))
+    ecom.add_foreign_key("events.user_id", "users.id")
+    ecom.add_foreign_key("order_items.user_id", "users.id")
+    ecom.add_foreign_key("order_items.inventory_item_id", "inventory_items.id")
+    ecom.add_foreign_key("inventory_items.product_id", "products.id")
+    ecom.add_foreign_key("inventory_items.product_distribution_center_id", "distribution_center.id")
+    ecom.add_foreign_key("products.distribution_center_id", "distribution_center.id")
+    return ecom
+
+
+def test_ecommerce_relational_data():
+    ecom = _setup_ecommerce()
+
+    assert ecom.get_parents("users") == []
+    assert ecom.get_parents("events") == ["users"]
+    assert set(ecom.get_parents("inventory_items")) == {"products", "distribution_center"}
+
+    users_with_ancestors = ecom.get_table_data_with_ancestors("users")
+    assert set(users_with_ancestors.columns) == {
+        "self|id", "self|first_name", "self|last_name"
+    }
+    restored_users = ecom.drop_ancestral_data(users_with_ancestors)
+    assert set(restored_users.columns) == {"id", "first_name", "last_name"}
+
+    events_with_ancestors = ecom.get_table_data_with_ancestors("events")
+    assert set(events_with_ancestors.columns) == {
+        "self|id", "self|browser", "self|traffic_source", "self|user_id",
+        "self.user_id|id", "self.user_id|first_name", "self.user_id|last_name",
+    }
+    restored_events = ecom.drop_ancestral_data(events_with_ancestors)
+    assert set(restored_events.columns) == {"id", "browser", "traffic_source", "user_id"}
+
+    inventory_items_with_ancestors = ecom.get_table_data_with_ancestors("inventory_items")
+    assert set(inventory_items_with_ancestors.columns) == {
+        "self|id", "self|sold_at", "self|cost", "self|product_id", "self|product_distribution_center_id",
+        "self.product_id|id", "self.product_id|name", "self.product_id|brand", "self.product_id|distribution_center_id",
+        "self.product_distribution_center_id|id", "self.product_distribution_center_id|name",
+        "self.product_id.distribution_center_id|id", "self.product_id.distribution_center_id|name",
+    }
+    restored_inventory_items = ecom.drop_ancestral_data(inventory_items_with_ancestors)
+    assert set(restored_inventory_items.columns) == {"id", "sold_at", "cost", "product_id", "product_distribution_center_id"}
+
+
+def test_mutagenesis_relational_data():
+    mutagenesis = RelationalData()
+    mutagenesis.add_table("bond", None, pd.DataFrame(columns=["type", "atom1_id", "atom2_id"]))
+    mutagenesis.add_table("atom", "atom_id", pd.DataFrame(columns=["atom_id", "element", "charge", "molecule_id"]))
+    mutagenesis.add_table("molecule", "molecule_id", pd.DataFrame(columns=["molecule_id", "mutagenic"]))
+    mutagenesis.add_foreign_key("bond.atom1_id", "atom.atom_id")
+    mutagenesis.add_foreign_key("bond.atom2_id", "atom.atom_id")
+    mutagenesis.add_foreign_key("atom.molecule_id", "molecule.molecule_id")
+
+    assert mutagenesis.get_parents("bond") == ["atom"]
+    assert mutagenesis.get_parents("atom") == ["molecule"]
+
+    assert mutagenesis.get_primary_key("bond") is None
+    assert mutagenesis.get_primary_key("atom") == "atom_id"
+
+    bond_with_ancestors = mutagenesis.get_table_data_with_ancestors("bond")
+    assert set(bond_with_ancestors.columns) == {
+        "self|type", "self|atom1_id", "self|atom2_id",
+        "self.atom1_id|atom_id", "self.atom1_id|element", "self.atom1_id|charge", "self.atom1_id|molecule_id",
+        "self.atom2_id|atom_id", "self.atom2_id|element", "self.atom2_id|charge", "self.atom2_id|molecule_id",
+        "self.atom1_id.molecule_id|molecule_id", "self.atom1_id.molecule_id|mutagenic",
+        "self.atom2_id.molecule_id|molecule_id", "self.atom2_id.molecule_id|mutagenic",
+    }
+    restored_bond = mutagenesis.drop_ancestral_data(bond_with_ancestors)
+    assert set(restored_bond.columns) == {"type", "atom1_id", "atom2_id"}
 
 
 def test_walk_ecommerce():
-    relationships = Relationships() \
-        .add(("events", "user_id"), ("users", "id")) \
-        .add(("products", "distribution_center_id"), ("distribution_center", "id")) \
-        .add(("inventory_items", "product_id"), ("products", "id")) \
-        .add(("inventory_items", "distribution_center_id"), ("distribution_center", "id")) \
-        .add(("order_items", "inventory_item_id"), ("inventory_items", "id")) \
-        .add(("order_items", "user_id"), ("users", "id"))
-    t = TableProgress(relationships)
+    ecom = _setup_ecommerce()
+    t = TableProgress(ecom)
 
     # To start, "eldest generation" tables (those with no parents / outbound foreign keys) are ready
     assert set(t.ready()) == {"users", "distribution_center"}
