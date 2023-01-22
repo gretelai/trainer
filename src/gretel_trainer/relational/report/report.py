d24aecfa77ebf6d55bbc6057a6fb005d3185ce11
Mike Knepper <mike@gretel.ai>
2023-01-22 13:33:52 -0600

    Refactor report

--- src/gretel_trainer/relational/report/report.py
+++ src/gretel_trainer/relational/report/report.py
@@ -1,99 +1,59 @@
+from __future__ import annotations
+
 import datetime
-import logging
+import itertools
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 import pandas as pd
-from jinja2 import Environment, FileSystemLoader, Template
+from jinja2 import Environment, FileSystemLoader
 
-from gretel_trainer.relational import MultiTable
+from gretel_trainer.relational.core import RelationalData, TableEvaluation
+from gretel_trainer.relational.multi_table import MultiTable
 
-logger = logging.getLogger(__name__)
-
-_TEMPLATE_PATH = str(Path(__file__).parent)
-
-file_loader = FileSystemLoader(_TEMPLATE_PATH)
-env = Environment(loader=file_loader)
-template = env.get_template("report_template.html")
-template.globals["now"] = datetime.datetime.utcnow()
+_TEMPLATE_DIR = str(Path(__file__).parent)
 
 
-class Report:
-    def __init__(self, multitable: MultiTable):
-        self.multitable = multitable
-        self.rel_data = multitable.relational_data
-        self.eval_dict = multitable.evaluations
-        self.fields_dict = self.create_fields_dict()
-        self.proj_name = multitable._project.display_name
-        self.tables = multitable.relational_data.list_all_tables()
-        self.num_tables = multitable.relational_data.debug_summary["table_count"]
-        self.num_fkeys = multitable.relational_data.debug_summary["foreign_key_count"]
-        self.max_depth = multitable.relational_data.debug_summary["max_depth"]
-        self.dir = multitable._working_dir
+def create_report(multitable: MultiTable) -> None:
+    presenter = ReportPresenter(
+        rel_data=multitable.relational_data,
+        evaluations=multitable.evaluations,
+        now=datetime.datetime.utcnow(),
+    )
+    output_path = multitable._working_dir / "multitable_report.html"
+    with open(output_path, "w") as report:
+        html_content = ReportRenderer().render(presenter)
+        report.write(html_content)
 
-    def create_fields_dict(self):
-        """
-        Output:
 
-        """
-        columns_data = []
-        tables = []
+class ReportRenderer:
+    def __init__(self):
+        file_loader = FileSystemLoader(_TEMPLATE_DIR)
+        env = Environment(loader=file_loader)
+        self.template = env.get_template("report_template.html")
 
-        # Start by assigning key colors
-        colors_dict = assign_key_colors(self.rel_data)
+    def render(self, presenter: ReportPresenter) -> str:
+        return self.template.render(presenter=presenter)
 
-        # For each table
-        for table in self.rel_data:
-            columns = []
-            tables.append(table)
 
-            # Add primary key, if exists
-            pkey = self.rel_data.get_primary_key(table)
-            if pkey is not None:
-                # Format cell class for pkey and append - bolded, color from color_dict
-                columns.append(Cell(value=pkey, color=colors_dict[table], bold="bold"))
+@dataclass
+class ReportPresenter:
+    rel_data: RelationalData
+    now: datetime.datetime
+    evaluations: Dict[str, TableEvaluation]
 
-            # Add any foreign keys
-            fkeys = self.rel_data.get_foreign_keys(table)
-            if len(fkeys) > 0:
-                for key in fkeys:
-                    # Append Cell class with fkey name, color of parent table
-                    columns.append(
-                        Cell(
-                            value=key.column_name,
-                            color=colors_dict[key.parent_table_name],
-                        )
-                    )
+    @property
+    def generated_at(self) -> str:
+        return self.now.strftime("%m/%d/%Y, %H:%M")
 
-            # Add in the remaining fields
-            report = self.eval_dict[table].individual_report_json
-            for item in report["fields"]:
-                # Append Cell class with name of field, no formatting
-                columns.append(Cell(value=item["name"]))
+    @property
+    def copyright_year(self) -> str:
+        return self.now.strftime("%Y")
 
-            # Add columns to columns_data array
-            columns_data.append(columns)
-        # Convert columns_data to dataframe, transpose, and fill N/As
-        cols_df = pd.DataFrame(columns_data, index=tables).T.fillna("")
-        # Convert df to dictionary
-        fields_dict = cols_df.to_dict("split")
-
-        return fields_dict
-
-    def render(self):
-        out = None
-        out_path = f"{self.dir}/multitable-report.html"
-        try:
-            out = template.render(report=self)
-            try:
-                with open(out_path, "w") as f:
-                    f.write(out)
-            except Exception:
-                logger.exception("Failed to save report.")
-        except Exception:
-            logger.exception("Failed to render report.")
-        return out
+    @property
+    def relationships(self) -> Relationships:
+        return _table_relationships(self.rel_data)
 
 
 @dataclass
@@ -102,27 +62,92 @@ class Cell:
     Dataclass to hold the formatting properties of the cell for rendering Table Relationships table
 
     Attributes:
-        value: str -  The value of the cell (in this case, the field name)
-        color: str -  Hexcode to use for background-color of cell, related keys are displayed with same color
-        bold: str - 'bold' if value is a primary key, else empty string
+        value (str): The text value of the cell
+        background_color (str, optional): Optional background color, in hexcode format
+        font_weight (str, optional): Optional modifier to font
     """
 
     value: str
-    color: str = ""
-    bold: str = ""
+    background_color: Optional[str] = None
+    font_weight: Optional[str] = None
+
+    @property
+    def style(self) -> str:
+        styles = []
+        if self.background_color is not None:
+            styles.append(f"background-color: {self.background_color}")
+        if self.font_weight is not None:
+            styles.append(f"font-weight: {self.font_weight}")
+        return "; ".join(styles)
 
 
-def assign_key_colors(rel_data):
-    colors = ["#ABEBC6", "#ABC6EB", "#EBABC6", "#EBC6AB", "#C6ABEB", "#C6EBAB"]
+@dataclass
+class Relationships:
+    columns: List[str]
+    data: List[Cell]
+
+
+def _table_relationships(rel_data: RelationalData) -> Relationships:
+    pk_colors = _assign_primary_key_colors(rel_data)
+
+    df_data = []
+    tables = []
+
+    for table in rel_data.list_all_tables():
+        tables.append(table)
+
+        seen = set()
+        fields = []
+
+        # Add primary key if present
+        pk = rel_data.get_primary_key(table)
+        if pk is not None:
+            seen.add(pk)
+            fields.append(
+                Cell(
+                    value=pk,
+                    background_color=pk_colors[table],
+                    font_weight="bold",
+                )
+            )
+
+        # Add foreign keys
+        fks = rel_data.get_foreign_keys(table)
+        for fk in fks:
+            seen.add(fk.column_name)
+            fields.append(
+                Cell(
+                    value=fk.column_name,
+                    background_color=pk_colors[fk.parent_table_name],
+                )
+            )
+
+        # Add other fields
+        for column in rel_data.get_table_data(table).columns:
+            if column not in seen:
+                fields.append(
+                    Cell(
+                        value=column,
+                    )
+                )
+
+        df_data.append(fields)
+
+    df = pd.DataFrame(df_data, index=tables).transpose().fillna("")
+    split = df.to_dict("split")
+
+    return Relationships(
+        columns=split["columns"],
+        data=split["data"],
+    )
+
+
+def _assign_primary_key_colors(rel_data: RelationalData) -> Dict[str, str]:
+    colors = itertools.cycle(
+        ["#ABEBC6", "#ABC6EB", "#EBABC6", "#EBC6AB", "#C6ABEB", "#C6EBAB"]
+    )
     color_dict = {}
-    i = 0
-    for table in rel_data:
-        if rel_data[table]["primary_key"] is not None:
-            color_dict[table] = colors[i]
-            if i < len(colors) - 1:
-                i = i + 1
-            else:
-                i = 0
-        else:
-            color_dict[table] = ""
+    for table in rel_data.list_all_tables():
+        if rel_data.get_primary_key(table) is not None:
+            color_dict[table] = next(colors)
     return color_dict
