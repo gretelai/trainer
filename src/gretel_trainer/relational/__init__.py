be596b739159faafba88651fa86f628c2cc6d05a
Mike Knepper <mike@gretel.ai>
2023-01-25 15:10:00 -0600

    Prevent duplicate logs in Colab (#40)

--- src/gretel_trainer/relational/__init__.py
+++ src/gretel_trainer/relational/__init__.py
@@ -24,6 +24,12 @@ log_levels = {
 log_format = "%(levelname)s - %(asctime)s - %(message)s"
 time_format = "%Y-%m-%d %H:%M:%S"
 
+# Clear out any existing root handlers
+# (This prevents duplicate log output in Colab)
+for root_handler in logging.root.handlers:
+    logging.root.removeHandler(root_handler)
+
+# Configure our loggers
 for name, level in log_levels.items():
     logger = logging.getLogger(name)
 
