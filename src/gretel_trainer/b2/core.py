758ad6ce310accbb5cd578cc27956b5e1c96e4d2
Mike Knepper <git@mikeknep.com>
2023-02-05 19:14:42 -0600

    Add b2 statuses and timer

--- src/gretel_trainer/b2/core.py
+++ src/gretel_trainer/b2/core.py
@@ -1,3 +1,4 @@
+import time
 from dataclasses import dataclass
 from enum import Enum
 from typing import Type, Union
@@ -38,3 +39,17 @@ class Datatype(str, Enum):
     tabular = "tabular"
     time_series = "time_series"
     natural_language = "natural_language"
+
+
+class Timer:
+    def __init__(self):
+        self.total_time = 0
+
+    def __enter__(self):
+        self.t = time.time()
+
+    def __exit__(self, type, value, traceback):
+        self.total_time = time.time() - self.t
+
+    def duration(self) -> float:
+        return round(self.total_time, 2)
---
+++ src/gretel_trainer/b2/status.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass, field
+from typing import Optional, Union
+
+import pandas as pd
+
+
+@dataclass
+class NotStarted:
+    @property
+    def display(self) -> str:
+        return "Not started"
+
+
+@dataclass
+class Skipped:
+    @property
+    def display(self) -> str:
+        return "Skipped"
+
+
+@dataclass
+class InProgress:
+    stage: str
+    train_secs: Optional[float] = None
+    generate_secs: Optional[float] = None
+
+    @property
+    def display(self) -> str:
+        return f"In progress ({self.stage})"
+
+
+@dataclass
+class Completed:
+    sqs: int
+    train_secs: float
+    generate_secs: float
+    synthetic_data: pd.DataFrame = field(repr=False)
+
+    @property
+    def display(self) -> str:
+        return "Completed"
+
+
+@dataclass
+class Failed:
+    during: str
+    # error: Exception
+    train_secs: Optional[float] = None
+    generate_secs: Optional[float] = None
+    synthetic_data: Optional[pd.DataFrame] = field(default=None, repr=False)
+
+    @property
+    def display(self) -> str:
+        return f"Failed ({self.during})"
+
+
+Status = Union[NotStarted, Skipped, InProgress, Completed, Failed]
