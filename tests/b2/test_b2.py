cd9a96e389a8b750c0918cd6a37cb509d5bad3cc
Mike Knepper <git@mikeknep.com>
2023-02-24 15:16:14 -0600

    wip: tests

---
+++ tests/b2/test_b2.py
@@ -0,0 +1,35 @@
+def test_run_with_gretel_dataset():
+    pass
+
+
+def test_run_with_custom_csv_dataset():
+    pass
+
+
+def test_run_with_custom_dataframe_dataset():
+    pass
+
+
+# parametrize
+def test_run_with_default_gretel_model():
+    pass
+
+
+def test_run_with_specific_gretel_model_config():
+    pass
+
+
+def test_run_with_custom_model():
+    pass
+
+
+def test_gptx_skips_too_many_columns():
+    pass
+
+
+def test_gptx_skips_non_natural_language_datatype():
+    pass
+
+
+def test_lstm_skips_datasets_with_over_150_columns():
+    pass
