aae4d2d6442d17a61d982747862d1b45783bc3c1
Mike Knepper <git@mikeknep.com>
2023-02-24 22:25:00 -0600

    GretelDatasets don't need to carry the pandas DF

--- src/gretel_trainer/b2/gretel_datasets.py
+++ src/gretel_trainer/b2/gretel_datasets.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import json
+from dataclasses import dataclass, field
 from functools import wraps
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import boto3
 import pandas as pd
@@ -17,7 +18,8 @@ class GretelDataset:
         self.name = name
         self.datatype = datatype
         self.tags = tags
-        self._df: Optional[pd.DataFrame] = None
+        self._row_count: Optional[int] = None
+        self._column_count: Optional[int] = None
 
     @property
     def data_source(self) -> str:
@@ -25,20 +27,21 @@ class GretelDataset:
 
     @property
     def row_count(self) -> int:
-        self._load_df()
-        return self._df.shape[0]  # type: ignore
+        return self._shape()[0]
 
     @property
     def column_count(self) -> int:
-        self._load_df()
-        return self._df.shape[1]  # type: ignore
+        return self._shape()[1]
 
-    def _load_df(self) -> None:
-        if self._df is None:
-            self._df = pd.read_csv(self.data_source)
+    def _shape(self) -> Tuple[int, int]:
+        if self._row_count is None or self._column_count is None:
+            df = pd.read_csv(self.data_source)
+            self._row_count = df.shape[0]
+            self._column_count = df.shape[1]
+        return self._row_count, self._column_count
 
     def __repr__(self) -> str:
-        return f"GretelDataset(name={self.name}, datatype={self.datatype}, tags={self.tags})"
+        return f"GretelDataset(name={self.name}, datatype={self.datatype})"
 
 
 class GretelDatasetRepo:
@@ -47,14 +50,14 @@ class GretelDatasetRepo:
         self.s3 = boto3.client(
             "s3", config=Config(signature_version=UNSIGNED), region_name="us-west-2"
         )
-        self.datasets = self._read_manifest()
+        self.manifest = self._read_manifest()
 
     def list_datasets(
         self,
         datatype: Optional[Union[Datatype, str]] = None,
         tags: Optional[List[str]] = None,
     ) -> List[GretelDataset]:
-        matches = list(self.datasets.values())
+        matches = list(self.manifest.values())
         if datatype is not None:
             matches = [dataset for dataset in matches if dataset.datatype == datatype]
         if tags is not None:
@@ -67,13 +70,13 @@ class GretelDatasetRepo:
 
     def get_dataset(self, name: str) -> GretelDataset:
         try:
-            return self.datasets[name]
+            return self.manifest[name]
         except KeyError:
             raise BenchmarkException(f"No dataset exists with name {name}")
 
     def list_tags(self) -> List[str]:
         unique_tags = set()
-        for dataset in self.datasets.values():
+        for dataset in self.manifest.values():
             for tag in dataset.tags:
                 unique_tags.add(tag)
         return list(unique_tags)
