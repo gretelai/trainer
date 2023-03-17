c73f523b56bc368adb7b9bedc703d499dd84284b
Mike Knepper <mike@gretel.ai>
2023-03-17 12:19:33 -0500

    Simplify logging setup

--- src/gretel_trainer/relational/__init__.py
+++ src/gretel_trainer/relational/__init__.py
@@ -1,10 +1,5 @@
 import logging
 
-# Some of these are imported simply to ensure the logger is instantiated before getting configured below
-import gretel_trainer.relational.sdk_extras
-import gretel_trainer.relational.strategies.ancestral
-import gretel_trainer.relational.strategies.common
-import gretel_trainer.relational.strategies.independent
 from gretel_trainer.relational.connectors import (
     Connector,
     mariadb_conn,
@@ -16,33 +11,19 @@ from gretel_trainer.relational.connectors import (
 from gretel_trainer.relational.core import RelationalData
 from gretel_trainer.relational.multi_table import MultiTable
 
-# Optimize logging for multitable output
-log_levels = {
-    "gretel_trainer.relational.connectors": "INFO",
-    "gretel_trainer.relational.core": "INFO",
-    "gretel_trainer.relational.multi_table": "INFO",
-    "gretel_trainer.relational.sdk_extras": "INFO",
-    "gretel_trainer.relational.strategies.ancestral": "INFO",
-    "gretel_trainer.relational.strategies.common": "INFO",
-    "gretel_trainer.relational.strategies.independent": "INFO",
-}
-
 log_format = "%(levelname)s - %(asctime)s - %(message)s"
 time_format = "%Y-%m-%d %H:%M:%S"
+formatter = logging.Formatter(log_format, time_format)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
 
 # Clear out any existing root handlers
 # (This prevents duplicate log output in Colab)
 for root_handler in logging.root.handlers:
     logging.root.removeHandler(root_handler)
 
-# Configure our loggers
-for name, level in log_levels.items():
-    logger = logging.getLogger(name)
-
-    formatter = logging.Formatter(log_format, time_format)
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-
-    logger.handlers.clear()
-    logger.addHandler(handler)
-    logger.setLevel(level)
+# Configure relational loggers
+logger = logging.getLogger("gretel_trainer.relational")
+logger.handlers.clear()
+logger.addHandler(handler)
+logger.setLevel("INFO")
