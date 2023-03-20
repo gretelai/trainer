9c33392d0fdf65c529a6b12349ad68e095e73ace
tylersbray <tyler.s.bray@gmail.com>
2023-03-20 22:56:57 +0000

    Add javascript for key highlighting.

---
+++ src/gretel_trainer/relational/report/key_highlight.js
@@ -0,0 +1,61 @@
+function mouseOverPk(event) {
+    event.preventDefault();
+    let target = event;
+
+    target.classList.add("label__highlighted");
+    target.parentNode.parentNode.classList.add("row__highlighted");
+
+    document.querySelector(`[data-fk-from=${target.id}]`).forEach(
+        fk => {
+            fk.classList.add("label__highlighted");
+            fk.parentNode.parentNode.classList.add("row__highlighted");
+        }
+    )
+}
+
+function mouseOutPk(event) {
+    event.preventDefault();
+    let target = event;
+
+    target.classList.remove("label__highlighted");
+    target.parentNode.parentNode.classList.remove("row__highlighted");
+
+    document.querySelector(`[data-fk-from=${target.id}]`).forEach(
+        fk => {
+            fk.classList.remove("label__highlighted");
+            fk.parentNode.parentNode.classList.remove("row__highlighted");
+        }
+    )
+}
+
+function mouseOverFk(event) {
+    event.preventDefault();
+    let target = event;
+
+    target.classList.add("label__highlighted");
+    target.parentNode.parentNode.classList.add("row__highlighted");
+
+    let pk = document.getElementById(target.getAttribute('data-fk-from'));
+    pk.classList.add("label__highlighted");
+    pk.parentNode.parentNode.classList.add("row__highlighted");
+}
+
+function mouseOutFk(event) {
+    event.preventDefault();
+    let target = event;
+
+    target.classList.remove("label__highlighted");
+    target.parentNode.parentNode.classList.remove("row__highlighted");
+
+    let pk = document.getElementById(target.getAttribute('data-fk-from'));
+    pk.classList.remove("label__highlighted");
+    pk.parentNode.parentNode.classList.remove("row__highlighted");
+}
+
+const pks = document.querySelectorAll('[data-pk]');
+pks.forEach(pk => { pk.onmouseover = mouseOverPk });
+pks.forEach(pk => { pk.onmouseout = mouseOutPk });
+
+const fks = document.querySelectorAll('[data-fk-from]');
+fks.forEach(fk => { fk.onmouseover = mouseOverFk });
+fks.forEach(fk => { fk.onmouseout = mouseOutFk });
