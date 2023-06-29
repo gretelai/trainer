53e3f951fbabbf09bb33292070c17b0e74adb48b
Mike Knepper <mike@gretel.ai>
2023-06-29 09:12:21 -0500

    Add deprecated gretel dataset functions

--- src/gretel_trainer/benchmark/__init__.py
+++ src/gretel_trainer/benchmark/__init__.py
@@ -4,6 +4,11 @@ from gretel_trainer.benchmark.comparison import Comparison, compare
 from gretel_trainer.benchmark.core import Datatype
 from gretel_trainer.benchmark.custom.datasets import make_dataset
 from gretel_trainer.benchmark.gretel.datasets import GretelDatasetRepo
+from gretel_trainer.benchmark.gretel.datasets_backwards_compatibility import (
+    get_gretel_dataset,
+    list_gretel_dataset_tags,
+    list_gretel_datasets,
+)
 from gretel_trainer.benchmark.gretel.models import (
     GretelACTGAN,
     GretelAmplify,
