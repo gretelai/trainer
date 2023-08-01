36b22638b690564c72de38b3804fbf97fdb5e4e4
Mike Knepper <mike@gretel.ai>
2023-08-01 09:29:32 -0500

    Move benchmark log setup out of init

--- src/gretel_trainer/benchmark/__init__.py
+++ src/gretel_trainer/benchmark/__init__.py
@@ -1,5 +1,3 @@
-import logging
-
 from gretel_trainer.benchmark.core import BenchmarkConfig, Datatype
 from gretel_trainer.benchmark.custom.datasets import create_dataset, make_dataset
 from gretel_trainer.benchmark.entrypoints import compare, launch
@@ -18,20 +16,4 @@ from gretel_trainer.benchmark.gretel.models import (
     GretelLSTM,
     GretelModel,
 )
-
-log_format = "%(levelname)s - %(asctime)s - %(message)s"
-time_format = "%Y-%m-%d %H:%M:%S"
-formatter = logging.Formatter(log_format, time_format)
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-# Clear out any existing root handlers
-# (This prevents duplicate log output in Colab)
-for root_handler in logging.root.handlers:
-    logging.root.removeHandler(root_handler)
-
-# Configure benchmark loggers
-logger = logging.getLogger("gretel_trainer.benchmark")
-logger.handlers.clear()
-logger.addHandler(handler)
-logger.setLevel("INFO")
+from gretel_trainer.benchmark.log import set_log_level
