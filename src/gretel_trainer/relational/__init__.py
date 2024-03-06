69b78ab712e9fcb1f1d93fdc45bdf25c7502ecc6
Mike Knepper <mike@gretel.ai>
2024-03-06 11:54:03 -0600

    Trainer client bump and deprecation msg
    GitOrigin-RevId: 3f9d6f91f108bd1c7c2360c5259570854f6704ef

--- src/gretel_trainer/relational/__init__.py
+++ src/gretel_trainer/relational/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 import gretel_trainer.relational.log
 
 from gretel_trainer.relational.connectors import (
@@ -12,3 +14,11 @@ from gretel_trainer.relational.core import RelationalData
 from gretel_trainer.relational.extractor import ExtractorConfig
 from gretel_trainer.relational.log import set_log_level
 from gretel_trainer.relational.multi_table import MultiTable
+
+logger = logging.getLogger(__name__)
+
+logger.warn(
+    "Relational Trainer is deprecated, and will be removed in the next Trainer release. "
+    "To transform and synthesize relational data, use Gretel Workflows. "
+    "Visit the docs to learn more: https://docs.gretel.ai/create-synthetic-data/workflows-and-connectors"
+)
