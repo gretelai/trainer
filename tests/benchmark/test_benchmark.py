42ae5b1e07027b84d9196f44d0b34af255073c62
Matt Kornfield <kornfield@gretel.ai>
2025-02-12 23:59:30 -0800

    [TASK] More local/ deprecation test fixes
    GitOrigin-RevId: 6f38c34522857b5d20260f309d4de0215b51f73d

--- tests/benchmark/test_benchmark.py
+++ tests/benchmark/test_benchmark.py
@@ -10,13 +10,7 @@ import pytest
 
 from gretel_client.projects.jobs import Status
 from gretel_client.projects.models import read_model_config
-from gretel_trainer.benchmark import (
-    BenchmarkConfig,
-    compare,
-    create_dataset,
-    Datatype,
-    GretelLSTM,
-)
+from gretel_trainer.benchmark import BenchmarkConfig, compare, create_dataset, Datatype
 from gretel_trainer.benchmark.core import Dataset
 from gretel_trainer.benchmark.gretel.models import GretelModel
 
@@ -51,32 +45,6 @@ class TailoredActgan(GretelModel):
         c = read_model_config("synthetics/tabular-actgan")
         c["models"][0]["actgan"]["params"]["epochs"] = 100
         return c
-
-
-class SharedDictLstm(GretelModel):
-    config = {
-        "schema_version": "1.0",
-        "name": "tabular-lstm",
-        "models": [
-            {
-                "synthetics": {
-                    "data_source": "__tmp__",
-                    "params": {
-                        "epochs": "auto",
-                        "vocab_size": "auto",
-                        "learning_rate": "auto",
-                        "batch_size": "auto",
-                        "rnn_units": "auto",
-                    },
-                    "generate": {"num_records": 5000},
-                    "privacy_filters": {
-                        "outliers": "auto",
-                        "similarity": "auto",
-                    },
-                }
-            }
-        ],
-    }
 
 
 def test_run_with_gretel_dataset(working_dir, project, evaluate_report_handle, iris):
@@ -201,7 +169,7 @@ def test_run_with_custom_dataframe_dataset(
     assert "synth_DoNothingModel-pets.csv" in working_dir_contents
 
 
-@pytest.mark.parametrize("benchmark_model", [GretelLSTM, TailoredActgan])
+@pytest.mark.parametrize("benchmark_model", [TailoredActgan])
 def test_run_happy_path_gretel_sdk(
     benchmark_model, working_dir, iris, project, evaluate_report_handle
 ):
@@ -265,33 +233,6 @@ def test_run_happy_path_gretel_sdk(
     pdtest.assert_frame_equal(df, session.results)
 
 
-def test_sdk_model_failure(working_dir, iris, project):
-    model = Mock(
-        status=Status.ERROR,
-        billing_details={"total_time_seconds": 30},
-    )
-    model.submit.return_value = model
-
-    project.create_model_obj.side_effect = [model]
-
-    session = compare(
-        datasets=[iris],
-        models=[GretelLSTM],
-        config=BenchmarkConfig(
-            working_dir=working_dir,
-        ),
-    ).wait()
-
-    assert len(session.results) == 1
-    result = session.results.iloc[0]
-    assert result["Model"] == "GretelLSTM"
-    assert result["Status"] == "Failed (train)"
-    assert result["SQS"] is None
-    assert result["Train time (sec)"] == 30
-    assert result["Generate time (sec)"] is None
-    assert result["Total time (sec)"] == 30
-
-
 def test_run_with_failures(working_dir, project, iris):
     session = compare(
         datasets=[iris],
@@ -303,83 +244,6 @@ def test_run_with_failures(working_dir, project, iris):
 
     assert len(session.results) == 2
     assert set(session.results["Status"]) == {"Failed (train)", "Failed (generate)"}
-
-
-def test_custom_gretel_model_configs_do_not_overwrite_each_other(
-    working_dir, project, iris, df
-):
-    model = Mock(
-        status=Status.ERROR,
-        billing_details={"total_time_seconds": 30},
-    )
-    model.submit.return_value = model
-    project.create_model_obj.return_value = model
-
-    pets = create_dataset(df, datatype="tabular", name="pets")
-
-    compare(
-        datasets=[iris, pets],
-        models=[SharedDictLstm],
-        config=BenchmarkConfig(
-            working_dir=working_dir,
-        ),
-    ).wait()
-
-    model_names = [
-        call.kwargs["model_config"]["name"]
-        for call in project.create_model_obj.call_args_list
-    ]
-
-    assert set(model_names) == {"SharedDictLstm-iris", "SharedDictLstm-pets"}
-
-
-def test_lstm_skips_datasets_with_over_150_columns(working_dir, project):
-    jumbo = pd.DataFrame(columns=list(range(151)))
-    dataset = create_dataset(jumbo, datatype=Datatype.TABULAR, name="skippy")
-
-    session = compare(
-        datasets=[dataset],
-        models=[GretelLSTM],
-        config=BenchmarkConfig(
-            working_dir=working_dir,
-        ),
-    ).wait()
-
-    assert len(session.results) == 1
-    assert_is_skipped(session.results.iloc[0])
-
-
-def test_compare_creates_job_specs(working_dir, project, iris):
-    with patch("gretel_trainer.benchmark.entrypoints._entrypoint") as entrypoint:
-        dataset = create_dataset(
-            pd.DataFrame(data={"foo": [1, 2, 3]}),
-            datatype=Datatype.TABULAR,
-            name="skippy",
-        )
-
-        lstm = GretelLSTM()
-        compare(
-            datasets=[iris, dataset],
-            models=[DoNothingModel, lstm],
-            config=BenchmarkConfig(
-                working_dir=working_dir,
-            ),
-        )
-
-    assert entrypoint.call_count == 1
-    args, kwargs = entrypoint.call_args
-    assert args == ()
-
-    job_tuples = kwargs["jobs"]
-    assert len(job_tuples) == 4
-
-    lstm_jobs = [t for t in job_tuples if t[1] == lstm]
-    assert len(lstm_jobs) == 2
-    assert {d.name for d, m in lstm_jobs} == {"iris", "skippy"}
-
-    do_nothing_jobs = [t for t in job_tuples if t[1] == DoNothingModel]
-    assert len(do_nothing_jobs) == 2
-    assert {d.name for d, m in do_nothing_jobs} == {"iris", "skippy"}
 
 
 def assert_is_skipped(result):
